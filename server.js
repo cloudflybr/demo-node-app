922f0969d5176e6ea7402fa6cd8c20660d0c4b54
cloudflybr <nick@cloudfly.com.br>
2026-08-07 15:57:18 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+   const port = process.env.PORT || 3000;
+
+   http.createServer((req, res) => {
+     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+     res.end('<h1>🚀 CloudFly demo app</h1><p>Deployed from GitHub via git push.</p>');
+   }).listen(port, () => console.log(`up on ${port}`));
